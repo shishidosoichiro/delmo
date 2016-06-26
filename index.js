eb13a03912b45f0db671dcbaa44dcdc133731fb7
Soichiro Shishido <shishidosoichiro@gmail.com>
2016-06-26 23:40:26 +0900

    post

---
+++ index.js
@@ -0,0 +1,235 @@
+'use strict';
+
+var _ = require('lodash');
+var flow = require('flow');
+
+var slice = function(array, begin, end){
+	return Array.prototype.slice.call(array, begin, end);
+};
+var parallel = function(){
+	var args = slice(arguments);
+	return function(data){
+		return Promise.all(args.map(function(f){
+			return f(data);
+		}));
+	};
+};
+var whether = function(test, trueSide, falseSide){
+	return function(data){
+		if (test(data)) return trueSide(data);
+		else            return falseSide(data);
+	};
+};
+var reject = function(message){
+	return function(params){
+		var err = new Error(message);
+		err.params = params;
+		return Promise.reject(err);
+	};
+};
+var resolve = Promise.resolve.bind(Promise);
+
+var defaults = {
+	id: function(data){
+		return data['id'];
+	},
+	validate: _.noop,
+	serialize: _.noop,
+	deserialize: _.noop,
+	bind: true
+};
+
+var Restful = function(options){
+	if (!(this instanceof Restful)) return new Restful(options);
+
+	options = _.defaults(options, defaults);
+
+	this.options = options;
+	this.req = options.req;
+	this.id = options.id;
+	this.validate = options.validate;
+	this.serialize = options.serialize;
+	this.deserialize = options.deserialize;
+
+	this._insert = flow(
+		resolve,
+		this.validate,
+		this.serialize,
+		this.req.post
+	);
+
+	this._update = flow(
+		resolve,
+		parallel(
+			this.id,
+			flow(
+				this.validate, 
+				this.serialize
+			)
+		),
+		_.spread(this.req.put)
+	);
+
+	this._remove = flow(
+		resolve,
+		this.id,
+		this.removeById
+	);
+
+	this._byId = flow(
+		resolve,
+		this.id,
+		this.req.get,
+		this.deserialize
+	);
+
+	this._get = flow(
+		resolve,
+		this.id,
+		this.byId
+	);
+
+	this._hasId = flow(
+		this.id,
+		_.isEmpty
+	);
+
+	this._save = whether(this.hasId,
+		_.flow(
+			resolve,
+			this.validate,
+			parallel(this.serialize, this.get),
+			whether(_.spread(_.isEqual),
+				reject('same object.'),
+				_.flow(_.head, this.req.put)
+			)
+		),
+		this.insert
+	);
+
+	// bind
+	if (options.bind) {
+		this.insert = this.insert.bind(this);
+		this.update = this.update.bind(this);
+		this.removeById = this.removeById.bind(this);
+		this.remove = this.remove.bind(this);
+		this.byId = this.byId.bind(this);
+		this.get = this.get.bind(this);
+		this.find = this.find.bind(this);
+		this.hasId = this.hasId.bind(this);
+		this.save = this.save.bind(this);
+	}
+};
+
+/**
+ * insert
+ * 
+ * var promise = this.insert(object);
+ */
+Restful.prototype.insert = function(data){
+	return this._insert(data);
+};
+
+/**
+ * update
+ * 
+ * var promise = this.update(object);
+ */
+Restful.prototype.update = function(data){
+	return this._update(data);
+};
+
+/**
+ * removeById
+ 
+ * var promise = this.removeById(id); 
+ */
+Restful.prototype.removeById = function(id){
+	return this.req.remove(id);
+};
+
+/**
+ * removeById
+ 
+ * var promise = this.remove(object);
+ */
+Restful.prototype.remove = function(data){
+	return this._remove(data);
+};
+
+/**
+ * byId
+ * 
+ * var promise = this.byId(object);
+ */
+Restful.prototype.byId = function(id){
+	return this._byId(id);
+};
+
+/**
+ * get
+ * 
+ * var promise = this.get(object);
+ */
+Restful.prototype.get = function(data){
+	return this._get(data);
+};
+
+/**
+ * find
+ * 
+ * var promise = this.find(query);
+ */
+Restful.prototype.find = function(path, query){
+	return this.req.get(path, query);
+};
+
+/**
+ * hasId
+ * 
+ * var boolean = this.hasId(data);
+ */
+Restful.prototype.hasId = function(data){
+	return this._hasId(data);
+};
+
+/**
+ * save
+ * 
+ * var promise = this.save(data);
+ */
+Restful.prototype.save = function(data){
+	return this._save(data);
+};
+//Restful.prototype.save = function(data){
+//	if (this.hasId(data)) {
+//		return this.get(data)
+//		.then(function(old){
+//			if (_.isEqual(old, data)) return Promise.reject('same object.');
+//			return this.update(data);
+//		})
+//	}
+//	else {
+//		return this.insert(data);
+//	}
+//};
+
+/**
+ * post
+ * 
+ * var promise = this.insert(object);
+ */
+Restful.prototype.post = function(path, data){
+	return this.req.post(path, data);
+};
+
+/**
+ * put
+ * 
+ * var promise = this.insert(object);
+ */
+Restful.prototype.put = function(path, data){
+	return this.req.put(path, data);
+};
+
+module.exports = Restful;
---
+++ test/index.js
@@ -0,0 +1,163 @@
+'use strict';
+
+var chai = require('chai');
+var expect = chai.expect;
+var should = chai.should();
+
+var _ = require('lodash');
+
+var Restful = require('../');
+
+var stub = {
+	post: _.noop,
+	put: _.noop,
+	get: _.noop,
+	remove: _.noop
+};
+
+describe('restful', function(){
+	var ThrowValidationError = new Restful({
+		req: stub,
+		validate: function(data){
+			throw new Error('validation error');
+		}
+	});
+	var RejectValidationError = new Restful({
+		req: stub,
+		validate: function(data){
+			return Promise.reject(new Error('validation error'));
+		}
+	});
+	var ThrowSerializationError = new Restful({
+		req: stub,
+		serialize: function(data){
+			throw new Error('serialization error');
+		}
+	});
+	var RejectSerializationError = new Restful({
+		req: stub,
+		serialize: function(data){
+			return Promise.reject(new Error('serialization error'));
+		}
+	});
+	var ThrowPostError = new Restful({
+		req: _.defaults({post: function(){
+			throw new Error('post error');
+		}}, stub)
+	});
+	var RejectPostError = new Restful({
+		req: _.defaults({post: function(){
+			return Promise.reject(new Error('post error'));
+		}}, stub)
+	});
+
+	describe('#insert', function(done){
+		it('should validate and serialize and http post', function(){
+			var user = {username: 'taro'};
+			var User = new Restful({
+				req: {
+					post: function(data){
+						data.should.deep.equal(user);
+						return data;
+					},
+					put: function(){},
+					get: function(){},
+					remove: function(){}
+				},
+				validate: function(data){
+					data.should.deep.equal(user);
+					return data;
+				},
+				serialize: function(data){
+					data.should.deep.equal(user);
+					return data;
+				}
+			});
+			User.insert(user)
+			.then(function(data){
+				data.should.deep.equal(user);
+				done();
+			})
+			.catch(done);
+		});
+		it('should return rejected Promise, If validate method throws a error.', function(done){
+			ThrowValidationError.insert({})
+			.then(done)
+			.catch(function(err){
+				err.message.should.equal('validation error');
+				done();
+			});
+		});
+		it('should return rejected Promise, If validate method return rejected Promise.', function(done){
+			RejectValidationError.insert({})
+			.then(done)
+			.catch(function(err){
+				err.message.should.equal('validation error');
+				done();
+			});
+		});
+		it('should return rejected Promise, If serialize method throws a error.', function(done){
+			ThrowSerializationError.insert({})
+			.then(done)
+			.catch(function(err){
+				err.message.should.equal('serialization error');
+				done();
+			});
+		});
+		it('should return rejected Promise, If serialize method return rejected Promise.', function(done){
+			RejectSerializationError.insert({})
+			.then(done)
+			.catch(function(err){
+				err.message.should.equal('serialization error');
+				done();
+			});
+		});
+		it('should return rejected Promise, If post method throws a error.', function(done){
+			ThrowPostError.insert({})
+			.then(done)
+			.catch(function(err){
+				err.message.should.equal('post error');
+				done();
+			});
+		});
+		it('should return rejected Promise, If post method return rejected Promise.', function(done){
+			RejectPostError.insert({})
+			.then(done)
+			.catch(function(err){
+				err.message.should.equal('post error');
+				done();
+			});
+		});
+	});
+
+	describe('#update', function(){
+		
+	});
+	describe('#removeById', function(){
+		
+	});
+	describe('#remove', function(){
+		
+	});
+	describe('#byId', function(){
+		
+	});
+	describe('#get', function(){
+		
+	});
+	describe('#find', function(){
+		
+	});
+	describe('#hasId', function(){
+		
+	});
+	describe('#save', function(){
+		
+	});
+	describe('#post', function(){
+		
+	});
+	describe('#put', function(){
+		
+	});
+});
